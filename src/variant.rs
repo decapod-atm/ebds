ba22873f7887d50568f64d20b06ac1dccbe90c04
EBDS Rust <134741054+ebds-rs@users.noreply.github.com>
2023-08-17 16:27:29 -0600

    variant: add `CommandVariant` enum
    Adds the `CommandVariant` enum type to represent command messages from
    the crate.

--- src/variant.rs
+++ src/variant.rs
@@ -1,3 +1,9 @@
+//! Message variant types for crate messages.
+//!
+//! Allows returning a single type that wraps various message types from the crate.
+
+mod command;
 mod reply;
 
+pub use command::*;
 pub use reply::*;
