77d3c283acae0bf361454a27cc89cc605cd8f712
EBDS Rust <134741054+ebds-rs@users.noreply.github.com>
2023-08-17 16:04:41 -0600

    macros: add `inner_enum` helper macro
    Adds the convenience macro `inner_enum` to test and deconstruct an enum
    with new-type variants. Greatly reduces verbose, redundant code.

--- src/macros.rs
+++ src/macros.rs
@@ -575,3 +575,44 @@ macro_rules! impl_default {
         }
     };
 }
+
+/// Provides convenience functions to deconstruct an enum with new-type variants.
+#[macro_export]
+macro_rules! inner_enum {
+    // macro variant for when the enum variant and its type are the same ident
+    ($ty:ident, $var:ident) => {
+        inner_enum!($ty, $var, $var);
+    };
+
+    // macro variant for when the enum variant and its type are potentially different
+    ($ty:ident, $var:ident, $var_ty:ident) => {
+        impl $ty {
+            ::paste::paste! {
+                #[doc = "Gets whether `" $ty "` is the variant `" $var "`."]
+                pub fn [<is_ $var:snake>](&self) -> bool {
+                    matches!(self, $ty::$var(_))
+                }
+
+                #[doc = "Gets a reference to `" $ty "` as the variant `" $var "`'s inner type `" $var_ty "`."]
+                pub fn [<as_ $var:snake>](&self) -> $crate::Result<&$var_ty> {
+                    use $crate::Error;
+
+                    match self {
+                        $ty::$var(ty) => Ok(ty),
+                        _ => Err(Error::failure(format!("have variant: {self}, expected: {}", $crate::std::any::type_name::<$var>()))),
+                    }
+                }
+
+                #[doc = "Converts `" $ty "` into the variant `" $var "`'s inner type `" $var_ty "`."]
+                pub fn [<into_ $var:snake>](self) -> $crate::Result<$var_ty> {
+                    use $crate::Error;
+
+                    match self {
+                        $ty::$var(ty) => Ok(ty),
+                        _ => Err(Error::failure(format!("have variant: {self}, expected: {}", $crate::std::any::type_name::<$var>()))),
+                    }
+                }
+            }
+        }
+    };
+}
